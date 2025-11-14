2fd21c116b9ab6b2166cca21e34c1c7e9fd08a11
David Peluola <peluoladavid22@gmail.com>
2025-11-14 13:23:24 +0000

    implement counter functionnality

--- sandwitch_shop/lib/main.dart
+++ sandwitch_shop/lib/main.dart
@@ -33,16 +33,16 @@ class _OrderScreenState extends State<OrderScreen> {
   int _quantity = 0;
 
   void _increaseQuantity() {
-  if (_quantity < widget.maxQuantity) {
-    setState(() => _quantity++);
+    if (_quantity < widget.maxQuantity) {
+      setState(() => _quantity++);
+    }
   }
-}
 
-void _decreaseQuantity() {
-  if (_quantity > 0) {
-    setState(() => _quantity--);
+  void _decreaseQuantity() {
+    if (_quantity > 0) {
+      setState(() => _quantity--);
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
@@ -62,13 +62,14 @@ void _decreaseQuantity() {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                  onPressed: () => print('Add button pressed!'),
+                  onPressed: _increaseQuantity,
                   child: const Text('Add'),
                 ),
                 ElevatedButton(
-                  onPressed: () => print('Remove button pressed!'),
+                  onPressed: _decreaseQuantity,
                   child: const Text('Remove'),
                 ),
+                
               ],
             ),
           ],
