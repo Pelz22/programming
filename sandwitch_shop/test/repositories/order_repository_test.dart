0411918c900e8485695f2abeea725c9583032a9e
David Peluola <peluoladavid22@gmail.com>
2025-11-21 05:41:15 +0000

    add repositories to test

---
+++ sandwitch_shop/test/repositories/order_repository_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sandwich_shop/repositories/order_repository.dart';
+
+void main() {
+  group('OrderRepository', () {
+    test('initial quantity should be 0', () {
+      final repository = OrderRepository(maxQuantity: 5);
+      expect(repository.quantity, 0);
+    });
+
+    test('increment should increase quantity by 1', () {
+      final repository = OrderRepository(maxQuantity: 5);
+      repository.increment();
+      expect(repository.quantity, 1);
+    });
+
+    test('decrement should decrease quantity by 1', () {
+      final repository = OrderRepository(maxQuantity: 5);
+      repository.increment(); // quantity is now 1
+      repository.decrement(); // quantity is now 0
+      expect(repository.quantity, 0);
+    });
+
+    test('quantity should not exceed maxQuantity', () {
+      final repository = OrderRepository(maxQuantity: 2);
+      repository.increment(); // quantity is 1
+      repository.increment(); // quantity is 2
+      repository.increment(); // should not change
+      expect(repository.quantity, 2);
+    });
+
+    test('quantity should not go below 0', () {
+      final repository = OrderRepository(maxQuantity: 5);
+      repository.decrement(); // should not change
+      expect(repository.quantity, 0);
+    });
+  });
+}
+//The tests in this file are grouped using the group function, which helps organise related tests together. Each individual test is defined using the test function. The general format of a unit test is:
+
+//test('description of the test', () {
+  // Arrange: set up any necessary objects or conditions
+  // final object = /* create object */;
+  // Act: perform the action you want to test
+  // object.someMethod();
+  // Assert: check that the expected outcome occurred
+  // expect(object.someProperty, expectedValue);
+//});
